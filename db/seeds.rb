464624d460291186d1bfab5e8f13f24eff234875
gerardmi24 <gerardmistretta@yahoo.com>
2021-02-10 22:04:55 -0500

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,11 +30,20 @@ sports = Sport.create([
 ])
 
 teams = Team.create([
-    {sport_id: sports[0].id, team_name: 'Cavaliers', city: 'Cleveland', roster: 'Delly, Sexton, Drummond, McGee, Okoro, Garland, Love, Allen, Prince, Wade, Stevens, Windler, Nance, Osman, Dotson', coach: "JB Bickerstaff", sport_name: sports[0].sport_name},
-    {sport_id: sports[1].id, team_name: 'Giants', city: 'New York', roster: 'Jones, Barkley, Engram, Gallman, Lawrence, Williams, Slayton, Martinez, Tate, Hernandez, Ryan, Bradberry, Gano, Peppers, Shepard, Ximines', coach: "Joe Judge", sport_name: sports[1].sport_name},
-    {sport_id: sports[2].id, team_name: 'Rangers', city: 'New York', roster: 'Bitetto, Lemieux, Kreider, Fox, Strome, Zibanejad, Rooney, Johnson, Gauthier, Panarin', coach: "David Quinn", sport_name: sports[2].sport_name},
-    {sport_id: sports[0].id, team_name: 'Knicks', city: 'New York', roster: 'Robinson, Toppin, Knox, Rose, Barrett, Gibson, Quickley, Peyton', coach: "Tom Thibs", sport_name: sports[0].sport_name},
+    {sport_id: sports[0].id, team_name: 'Hawks', city: 'Atlanta', roster: 'Young, Capela, Collins, Rondo, Reddish, Gallinari, Huerter', coach: "Lloyd Pierce", sport_name: sports[0].sport_name},
+    {sport_id: sports[0].id, team_name: 'Celtics', city: 'Boston', roster: 'Tatum, Walker, Brown, Thompson, Teague, Fall, Theis, Edwards', coach: "Brad Stevens", sport_name: sports[0].sport_name},
     {sport_id: sports[0].id, team_name: 'Nets', city: 'Brooklyn', roster: 'Durant, Harden, Irving, Harris, Jordan, Claxton, TLC', coach: "Steve Nash", sport_name: sports[0].sport_name},
+    {sport_id: sports[0].id, team_name: 'Cavaliers', city: 'Cleveland', roster: 'Delly, Sexton, Drummond, McGee, Okoro, Garland, Love, Allen, Prince, Windler, Nance, Osman, Dotson', coach: "JB Bickerstaff", sport_name: sports[0].sport_name},
+    {sport_id: sports[0].id, team_name: 'Mavericks', city: 'Dallas', roster: 'Doncic, Porzingis, Richardson, Hardaway Jr, Kleber, Brunson, Finney-Smith', coach: "Rick Carlisle", sport_name: sports[0].sport_name},
+    {sport_id: sports[0].id, team_name: 'Knicks', city: 'New York', roster: 'Robinson, Toppin, Knox, Rose, Barrett, Gibson, Quickley, Peyton', coach: "Tom Thibs", sport_name: sports[0].sport_name},
+    {sport_id: sports[2].id, team_name: 'Bruins', city: 'Boston', roster: 'Pastrnak, DeBrusk, Rask, Smith, Marchand, Bergeron, McAvoy', coach: "Bruce Cassidy", sport_name: sports[2].sport_name},
+    {sport_id: sports[2].id, team_name: 'Stars', city: 'Dallas', roster: 'Seguin, Benn, Pavelski, Bishop, Radulov, Hintz', coach: "Rick Bowness", sport_name: sports[2].sport_name},
+    {sport_id: sports[2].id, team_name: 'Islanders', city: 'New York', roster: 'Barzal, Sorokin, Martin, Aho, Lee, Varlamov, Schneider, Wahlstrom', coach: "Barry Trotz and Jim Hiller", sport_name: sports[2].sport_name},
+    {sport_id: sports[2].id, team_name: 'Rangers', city: 'New York', roster: 'Bitetto, Lemieux, Kreider, Fox, Strome, Zibanejad, Rooney, Johnson, Gauthier, Panarin', coach: "David Quinn", sport_name: sports[2].sport_name},
+    {sport_id: sports[1].id, team_name: 'Falcons', city: 'Atlanta', roster: 'Ryan, Jones, Gurley, Hurst, Ridley, Fowler Jr, Neal, Dennard, Koo', coach: "Arthur Smith", sport_name: sports[1].sport_name},
+    {sport_id: sports[1].id, team_name: 'Browns', city: 'Cleveland', roster: 'Mayfield, Chubb, Hunt, Garrett, Landry, Ward, Vernon, Beckham Jr', coach: "Kevin Stefanski", sport_name: sports[1].sport_name},
+    {sport_id: sports[1].id, team_name: 'Cowboys', city: 'Dallas', roster: 'Prescott, Elliott, Lamb, Cooper, Vander Esch, Smith, Lee, Gregory, Martin', coach: "Mike McCarthy", sport_name: sports[1].sport_name},
+    {sport_id: sports[1].id, team_name: 'Giants', city: 'New York', roster: 'Jones, Barkley, Engram, Gallman, Lawrence, Williams, Slayton, Martinez, Hernandez, Bradberry, Peppers, Shepard, Ximines', coach: "Joe Judge", sport_name: sports[1].sport_name},
     {sport_id: sports[1].id, team_name: 'Jets', city: 'New York', roster: 'Darnold, Maye, Mims, Flacco, Gore, Crowder, Mosley, Perriman, Ficken', coach: "Robert Saleh", sport_name: sports[1].sport_name},
 ])
 
